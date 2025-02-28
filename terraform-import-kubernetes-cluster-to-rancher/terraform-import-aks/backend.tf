634ce6b631816f143e65aaeadae98eaf3769306f
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-02-28 20:08:42 +0530

    Update backend.tf

--- terraform-import-kubernetes-cluster-to-rancher/terraform-import-aks/backend.tf
+++ terraform-import-kubernetes-cluster-to-rancher/terraform-import-aks/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "terraformstate08022024"
     container_name       = "terraform-state"
     key                  = "aks-downstream-1-import/terraform.tfstate"
+    subscription_id      = "5XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
   }
 }
