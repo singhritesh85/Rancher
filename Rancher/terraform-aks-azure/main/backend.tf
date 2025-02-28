283bdc5ae9033e211b0adf4ad54056d563084f5b
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-02-28 20:07:42 +0530

    Update backend.tf

--- Rancher/terraform-aks-azure/main/backend.tf
+++ Rancher/terraform-aks-azure/main/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "terraformstate08022024"
     container_name       = "terraform-state"
     key                  = "state/terraform.tfstate"
+    subscription_id      = "5XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
   }
 }
