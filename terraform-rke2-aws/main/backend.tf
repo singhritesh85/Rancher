76a5350cffcfecbed5d5c5f808017dbabb1991cb
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-12-06 22:35:10 +0530

    Delete terraform-rke2-aws directory

--- terraform-rke2-aws/main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket         = "dolo-dempo"
-   key            = "state/rke2/terraform.tfstate"
-   region         = "us-east-2"
-   encrypt        = true
-   use_lockfile   = true   ###dynamodb_table = "terraform-state"
- }
-}
