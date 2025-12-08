6cb239ace53e1d9a1d41cbc615d5dd9075390e4e
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-12-08 19:04:25 +0530

    Add files via upload

---
+++ terraform-rke2-aws/main/user_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/usr/sbin/useradd -s /bin/bash -m ritesh;
+mkdir /home/ritesh/.ssh;
+chmod -R 700 /home/ritesh;
+echo "ssh-rsa XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ritesh@DESKTOP-0XXXXXX" >> /home/ritesh/.ssh/authorized_keys;
+chmod 600 /home/ritesh/.ssh/authorized_keys;
+chown ritesh:ritesh /home/ritesh/.ssh -R;
+echo "ritesh  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ritesh;
+chmod 440 /etc/sudoers.d/ritesh;
+
+############# Install Helm ################
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 --output-dir ~/
+chmod 700 ~/get_helm.sh
+~/get_helm.sh
+
+helm version
+
+#################################### Installation of Rsyslog ###########################################
+
+yum install rsyslog -y
+systemctl start rsyslog
+systemctl enable rsyslog
+systemctl status rsyslog
+
